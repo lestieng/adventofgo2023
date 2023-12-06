0c99b6bb58f13c1aa5e055bb1011b95da4c604fd
lestieng <lestieng@gmail.com>
2023-12-06 12:19:17 +0100

    more cleanup

--- day6/day6.go
+++ day6/day6.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	// "slices"
 	"strconv"
 )
 
@@ -17,14 +16,12 @@ func get_numbers(line string,part int) (nums []float64) {
         line = strings.ReplaceAll(line," ","")
     }
     reg := regexp.MustCompile(`\d+`)
-    num_strs := reg.FindAllString(line,-1)
-    for _,str := range num_strs {
+    for _,str := range reg.FindAllString(line,-1) {
         num,_ := strconv.ParseFloat(str,64)
         nums = append(nums,num)
     }
     return nums
 }
-
 
 func main() {
     file,err := os.Open(os.Args[1])
@@ -39,29 +36,25 @@ func main() {
     }
 
     scanner := bufio.NewScanner(file)
-    _ = scanner.Scan()
-    times := get_numbers(scanner.Text(),part)
-    _ = scanner.Scan()
-    distances := get_numbers(scanner.Text(),part)
+    scanner.Scan(); times := get_numbers(scanner.Text(),part)
+    scanner.Scan(); distances := get_numbers(scanner.Text(),part)
     
     ans := 1
     for i,time := range times { // solve second degree equation for speed zero-crossing
-        if discriminant := time*time - 4.0*distances[i]; discriminant > 0 {
+        if discriminant := time*time - 4.0*distances[i]; discriminant > 0 { // make sure solution exists
             discr_sqrt := math.Sqrt(discriminant)
-            term1 := int(math.Ceil(0.5*(time - discr_sqrt)))
-            term2 := int(math.Floor(0.5*(time + discr_sqrt)))
+            solution1 := int(math.Ceil(0.5*(time - discr_sqrt)))
+            solution2 := int(math.Floor(0.5*(time + discr_sqrt)))
             // test for edge case when you zero-cross at an integer exactly
             // when sqrt is an integer AND time is even (so the sqrt and sum is even)
             if discr_sqrt - math.Round(discr_sqrt) < math.SmallestNonzeroFloat64 && 
                 math.Mod(time,2.0) < math.SmallestNonzeroFloat64 {
-                ans *= term2 - term1 - 1
+                ans *= solution2 - solution1 - 1
             } else {
-                ans *= term2 - term1 + 1
+                ans *= solution2 - solution1 + 1
             }
         }
     }
 
     fmt.Println("The answer is: ",ans)
-
-
 }
