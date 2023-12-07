29b717cda1e76d636c39d9614dfe321167629c5b
lestieng <lestieng@gmail.com>
2023-12-07 19:41:38 +0100

    cleanup

--- day7/day7.go
+++ day7/day7.go
@@ -22,6 +22,16 @@ var ofAKind = map[int]int {
     2: ONE_PAIR, 3: THREE_OF_KIND, 4: FOUR_OF_KIND, 5: FIVE_OF_KIND,
 }
 
+var cardValues = map[rune]int {
+    'A': 12, 'K': 11, 'Q': 10, 'J': 9, 'T': 8, '9': 7, '8': 6, '7': 5, 
+    '6': 4, '5': 3, '4': 2, '3': 1, '2': 0,
+}
+
+var cardValues2 = map[rune]int {
+    'A': 12, 'K': 11, 'Q': 10, 'J': 9, 'T': 8, '9': 7, '8': 6, '7': 5, 
+    '6': 4, '5': 3, '4': 2, '3': 1, '2': 0,
+}
+
 type cardHand struct {
     card_scores []int
     bid int 
@@ -65,8 +75,7 @@ func set_type_score2(init_score int,type_tracker []int,jcount int) (type_score i
     return type_score
 } 
 
-func set_scores(hand string, cardValues map[rune]int) (
-                type_score int, card_scores []int) {
+func set_scores1(hand string) (type_score int, card_scores []int) {
     type_tracker := make([]int,0)
     remaining := hand
     for _,c := range hand {
@@ -82,13 +91,12 @@ func set_scores(hand string, cardValues map[rune]int) (
     return set_type_score1(type_tracker),card_scores
 }
 
-func set_scores2(hand string, cardValues map[rune]int) (
-                type_score int, card_scores []int) {
+func set_scores2(hand string) (type_score int, card_scores []int) {
     type_tracker := make([]int,0)
     jcount := 0
     remaining := hand
     for _,c := range hand {
-        card_scores = append(card_scores,cardValues[c])
+        card_scores = append(card_scores,cardValues2[c])
         if c == 'J' { jcount++ }
         if len(remaining) > 0 {
             count := strings.Count(remaining,string(c))
@@ -105,18 +113,18 @@ func set_scores2(hand string, cardValues map[rune]int) (
     return type_score,card_scores
 }
 
-func set_hand(hand *cardHand, line string, cardValues map[rune]int) {
+func set_hand(hand *cardHand, line string) {
     base_split := strings.Split(line," ")
     hand.hand = base_split[0]
     hand.bid,_ = strconv.Atoi(base_split[1])
-    hand.type_score,hand.card_scores = set_scores(hand.hand,cardValues)
+    hand.type_score,hand.card_scores = set_scores1(hand.hand)
 }
 
-func set_hand2(hand *cardHand, line string, cardValues map[rune]int) {
+func set_hand2(hand *cardHand, line string) {
     base_split := strings.Split(line," ")
     hand.hand = base_split[0]
     hand.bid,_ = strconv.Atoi(base_split[1])
-    hand.type_score,hand.card_scores = set_scores2(hand.hand,cardValues)
+    hand.type_score,hand.card_scores = set_scores2(hand.hand)
 }
 
 func should_insert(newHand cardHand, oldHand cardHand) bool {
@@ -132,57 +140,36 @@ func should_insert(newHand cardHand, oldHand cardHand) bool {
 func insert_sorted(hands []cardHand,newHand cardHand) (newHands []cardHand) {
     if len(hands) == 0 { return append(hands,newHand) }
     for i,hand := range hands {
-        if should_insert(newHand,hand) {
-            newHands = slices.Insert(hands,i,newHand)
-            return newHands
-        }
+        if should_insert(newHand,hand) { return slices.Insert(hands,i,newHand) }
     }
-    newHands = append(hands,newHand)
-    return newHands
+    return append(hands,newHand)
 }
 
 func main(){
-
-    part,err := strconv.Atoi(os.Args[1])
+    file,err := os.Open(os.Args[1])
     if err != nil {
-        log.Fatal("problem parsing cmd line arg",err)
-    }
-    var file *os.File
-    if part == 0 {
-        file,err = os.Open("test_input.txt")
-    } else {
-        file,err = os.Open("day7_input.txt")
-    }
-    if err != nil {
-        log.Fatal("problem or opening file",err)
+        log.Fatal("problem parsing first cmd line arg (or opening file)",err)
     }
     defer file.Close()
 
-    var cardValues map[rune]int
+    part,err := strconv.Atoi(os.Args[2])
+    if err != nil {
+        log.Fatal("problem parsing second cmd line arg",err)
+    }
+    defer file.Close()
+
     Hands := make([]cardHand,0)
 
     if part == 1 {
-        cardValues = map[rune]int {
-            'A': 12, 'K': 11, 'Q': 10, 'J': 9, 
-            'T': 8, '9': 7, '8': 6, '7': 5, 
-            '6': 4, '5': 3, '4': 2, '3': 1, 
-            '2': 0,
-        }
         for scanner := bufio.NewScanner(file); scanner.Scan();  {
             newHand := cardHand{}
-            set_hand(&newHand,scanner.Text(),cardValues)
+            set_hand(&newHand,scanner.Text())
             Hands = insert_sorted(Hands,newHand)
         }
     } else {
-        cardValues = map[rune]int {
-            'A': 12, 'K': 11, 'Q': 10, 'T': 9,
-            '9': 8, '8': 7, '7': 6, '6': 5, 
-            '5': 4, '4': 3, '3': 2, '2': 1, 
-            'J': 0, 
-        }
         for scanner := bufio.NewScanner(file); scanner.Scan();  {
             newHand := cardHand{}
-            set_hand2(&newHand,scanner.Text(),cardValues)
+            set_hand2(&newHand,scanner.Text())
             Hands = insert_sorted(Hands,newHand)
         }
     }
@@ -193,5 +180,4 @@ func main(){
     }
 
     fmt.Println("The answer is: ",ans)
-
 }
